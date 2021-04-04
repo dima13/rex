6fac807ccc2069291c49cbc6473e02b43e74745b
Tretiakov Stanislav <tretyakovpro4@gmail.com>
2021-04-04 23:35:36 +0300

    soap

--- soap/server.js
+++ soap/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const soap = require('soap')
 
 let myService = {
@@ -20,8 +19,6 @@ let xml = require('fs').readFileSync('./soap/myservice.wsdl', 'utf8')
 
 //express server example
 let app = express()
-//body parser middleware are supported (optional)
-app.use(bodyParser.raw({ type: function () { return true }, limit: '5mb' }))
 app.listen(8001, function () {
     //Note: /wsdl route will be handled by soap module
     //and all other routes & middleware will continue to work
